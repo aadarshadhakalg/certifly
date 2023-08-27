1987ad4b78aaec4f366644aef8569fc522f757db
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-08-27 08:37:06 +0545

    scaling and resolution retaining successful

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	let displayCertificate = '';
 	let isTemplateLoaded = false;
+	let origDimensions: { width: number; height: number } = { width: 0, height: 0 };
+
+	let templateCanvas: HTMLCanvasElement;
+	let templateDisplay: HTMLImageElement;
 
 	const handleFileInput = (e: Event) => {
 		const target = e.target as HTMLInputElement;
@@ -12,8 +15,39 @@
 		reader.readAsDataURL(file);
 		reader.onloadend = () => {
 			isTemplateLoaded = true;
-			displayCertificate = reader.result as string;
+
+			const ctx = templateCanvas.getContext('2d');
+			const img = new Image();
+			img.src = reader.result as string;
+			templateCanvas.width = img.width;
+			templateCanvas.height = img.height;
+			origDimensions = { width: img.width, height: img.height };
+			ctx?.drawImage(img, 0, 0);
+
+			// display template
+			templateDisplay.src = templateCanvas.toDataURL() as string;
+			templateDisplay.addEventListener('click', templateDisplayClickHandler);
 		};
+	};
+
+	const templateDisplayClickHandler = (event: MouseEvent) => {
+		const boundingRect = templateDisplay.getBoundingClientRect();
+		const offsetX = event.clientX - boundingRect.left;
+		const offsetY = event.clientY - boundingRect.top;
+
+		const offsetXRelative = offsetX / boundingRect.width;
+		const offsetYRelative = offsetY / boundingRect.height;
+
+		const offsetXCanvas = offsetXRelative * origDimensions.width;
+		const offsetYCanvas = offsetYRelative * origDimensions.height;
+
+		const ctx = templateCanvas.getContext('2d');
+		if (!ctx) {
+			return;
+		}
+		ctx.font = '14px Cursive';
+		ctx.fillText('Hello World', offsetXCanvas, offsetYCanvas);
+		templateDisplay.src = templateCanvas.toDataURL() as string;
 	};
 </script>
 
@@ -29,7 +63,8 @@
 	accept="image/png, image/jpeg"
 	on:change={handleFileInput}
 />
-<img id="display-template" src={displayCertificate} alt="template-display" />
+<img id="display-template" bind:this={templateDisplay} alt="template-display" />
+<canvas bind:this={templateCanvas} class="hidden" />
 
 <style>
 	#display-template {
